e580d5ec36e91c981421dff5435e3481472b93db
Spencer Kraisler <spencerkraisler@gmail.com>
2023-09-29 14:36:06 -0700

    updated bertsekas algo

--- scripts/bertsekas_auction_algo.py
+++ scripts/bertsekas_auction_algo.py
@@ -13,20 +13,19 @@ num_agents = 5
 num_tasks = num_agents
 
 # the assignment
-S = [i for i in range(num_agents)] # initially assign agent i to task i
+assignment = dict()
+unassigned_agents = {i for i in range(num_agents)}
 benefits = np.random.randint(0,10,(num_agents,num_tasks))
-epsilon = 1e-6 # min bid increment
+epsilon = 1e-3 # min bid increment
 prices = np.zeros(num_agents) # task prices
 
 util_hist = []
-for t in range(100):
-    total_util = 0
-    for i in range(num_agents):
-        total_util += benefits[i, S[i]]
-    util_hist.append(total_util)
-
+while True:
+    print(assignment)
+    if len(unassigned_agents) == 0:
+        break
     bids = dict() # bids is kept in a dict for ease 
-    for i in range(num_agents):
+    for i in unassigned_agents:
         # bidding process
         values_i = benefits[i,:] - prices
         j_i = np.argmax(values_i)
@@ -34,18 +33,33 @@ for t in range(100):
         bids[i, j_i] = benefits[i,j_i] - w_i + epsilon
 
     for j in range(num_tasks):
-        # assignments process
+        # assignment process
         P_j = []
         for pair in bids.keys():
             if pair[1] == j:
                 P_j.append(pair[0])
         if len(P_j) > 0:
             prices[j] = max({bids[i,j] for i in P_j})
-            S[P_j[0]] = j
+            i_j = P_j[0]
+            for i in [i for i,v in assignment.items() if v == j]:
+                unassigned_agents.add(i)
+                del assignment[i]
+            assignment[i_j] = j
+            unassigned_agents.remove(i_j)
 
-plt.plot(util_hist)
-plt.grid()
-plt.show()
 
-# Spencer: I think there is some issue in that we aren't converging to a single 
-# assignment, but maybe that is part of the algorithm. WIP. 
+def solve_centralized(self):
+        row_ind, col_ind = scipy.optimize.linear_sum_assignment(self.benefits, maximize=True)
+        if self.verbose:
+            print("Centralized solution")
+            for row, col in zip(row_ind, col_ind):
+                print(f"Agent {row} chose task {col_ind[row]} with benefit {self.benefits[row, col]}")
+            print(f"Total benefit: {self.benefits[row_ind, col_ind].sum()}")
+
+        return row_ind, col_ind
+
+cost = 0
+for i, j in assignment.items():
+    print(f"agent {i} is assigned to task {j}")
+    cost += benefits[i,j]
+print(cost)
