96f551a2061ccc1db6dd88e87b9be02c9e640b23
Spencer Kraisler <spencerkraisler@gmail.com>
2023-10-10 14:27:06 -0700

    finally got my dist algo working

--- scripts/spencer_dist_auction.py
+++ scripts/spencer_dist_auction.py
@@ -1,67 +1,72 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import methods
-import scipy.optimize
-import random
+import networkx as nx
 
-num_agents = 5
-num_tasks = 8
+num_agents = 100
+num_tasks = 200
+G = methods.rand_connected_graph(num_agents)
+Delta = nx.diameter(G)
+print(Delta)
+benefits = np.random.rand(num_agents, num_tasks)
+prices = np.zeros((num_agents, num_tasks))
+assignment = [i for i in range(num_agents)]
 
-
-
-"""
-beta = np.random.rand(num_agents, num_tasks)
-p = np.zeros((num_agents, num_tasks))
-alpha = np.arange(0, num_agents, 1).astype('int')
-b = np.zeros((num_agents, num_tasks)).astype('int')
-epsilon = .01
-next_p = np.zeros((num_agents, num_tasks))
-next_b = np.zeros((num_agents, num_tasks)).astype('int')
-next_alpha = np.zeros(num_agents)
-
+bidders = [[-1]*num_tasks]*num_agents
+epsilon = 1e-5/num_agents
+next_prices = prices
+next_bidders = bidders
+next_assignment = [0]*num_agents
 tol = 1e-5
-cost = []
-for t in range(100):
-    
-    total_assignment_benefit = 0
-    for i in range(num_agents):
-        total_assignment_benefit += beta[i, alpha[i]]
-    cost.append(total_assignment_benefit)
-    print(t,alpha,total_assignment_benefit)  
+unchanged_prices_count = 0
+rounds_count = 0
+while unchanged_prices_count < Delta:
+    flag = False
     for i in range(num_agents):
         N_i = list(G.neighbors(i))
         N_i.append(i)
+        N_i = sorted(N_i)
         for j in range(num_tasks):
-            # update prices
-            next_p[i,j] = np.max(p[N_i, j])
-
-            # update highest bidders
-            max_value = next_p[i,j]
-            max_indices = np.argwhere(abs(max_value - p[N_i,j]) < tol)[:,0]
-            next_b[i,j] = np.max(b[max_indices, j])
-            next_b = next_b.astype('int')
-
-        if p[i, alpha[i]] <= next_p[i, alpha[i]] and next_b[i, alpha[i]] != i:
-            max_value = np.max(beta[i,:] - next_p[i,:])
-            L = np.argwhere(abs(max_value - (beta[i,:] - next_p[i,:])) < tol)
-            random.shuffle(L)
-            next_alpha[i] = L[0]
-            next_alpha = next_alpha.astype('int')
-            next_b[i, next_alpha[i]] = i
-            L = list(beta[i,:] - p[i,:])
-            L.sort()
-            v_i = L[-1]
-            w_i = L[-2]
-            gamma_i = v_i - w_i + epsilon
-            next_p[i, next_alpha[i]] = p[i, next_alpha[i]] + gamma_i
+            max_price = np.max(prices[N_i, j])
+            next_prices[i,j] = max_price
+            highest_bidders = [
+                k for k in N_i if max_price - prices[k,j] < tol
+            ]
+            max_bidders = [bidders[k][j] for k in highest_bidders]
+            next_bidders[i][j] = max(max_bidders)
+        if prices[i, assignment[i]] <= next_prices[i, assignment[i]] \
+            and bidders[i][assignment[i]] != i:
+            flag = True
+            values = benefits[i,:] - next_prices[i,:]
+            max_value = max(values)
+            most_valuable_tasks = [
+                j for j in range(num_tasks) if max_value - values[j] < tol
+            ]
+            next_assignment[i] = most_valuable_tasks[0]
+            next_bidders[i][next_assignment[i]] = i
+            values = sorted(values)
+            v = max_value
+            w = sorted(values)[-2]
+            gamma = v - w + epsilon
+            next_prices[i, next_assignment[i]] \
+                = prices[i, next_assignment[i]] + gamma
         else:
-            next_alpha[i] = int(alpha[i])
-    p = next_p
-    b = next_b.astype('int')
-    alpha = next_alpha.astype('int')
-out = solve_centralized(beta)
-print(out)
-plt.plot(cost)
-plt.grid()
-plt.show()
-"""
+            next_assignment[i] = assignment[i]
+    assignment = next_assignment
+    if flag:
+        unchanged_prices_count = 0
+    else:
+        unchanged_prices_count += 1
+    prices = next_prices
+    bidders = next_bidders
+    rounds_count += 1
+    print(rounds_count, unchanged_prices_count)
+
+# centeralized comptuation 
+opt_assignment = methods.solve_centralized(benefits)
+print("opt: ", np.round(methods.cost(benefits, opt_assignment), 3))
+print("dist: ", np.round(methods.cost(benefits, assignment), 3))
+
+# plt.plot(cost)
+# plt.grid()
+# plt.show()
