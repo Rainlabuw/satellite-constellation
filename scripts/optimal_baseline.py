30cafa668a896d27c0ee1f5be15f7bb023b1b6a1
Josh Holder <jgh8@rice.edu>
2023-11-02 02:10:38 -0700

    optimal baseline script

--- scripts/optimal_baseline.py
+++ scripts/optimal_baseline.py
@@ -3,27 +3,7 @@ from methods import *
 import itertools
 import time
 
-def generate_matrices(n, m, T):
-    # This function will generate all possible matrices for given dimensions
-    max = 2**(n * m * T)
-    for i in range(max):
-        print(f"{i/max}", end='\r')
-        matrix = [[[0 for _ in range(T)] for _ in range(m)] for _ in range(n)]
-        # Convert i to binary and fill the matrix
-        bin_rep = format(i, f'0{n*m*T}b')
-        idx = 0
-        for x in range(n):
-            for y in range(m):
-                for z in range(T):
-                    matrix[x][y][z] = int(bin_rep[idx])
-                    idx += 1
-        matrix = np.array(matrix)
-
-        if not np.all(np.sum(matrix, axis=0) == 1) or not np.all(np.sum(matrix, axis=1) == 1):
-            continue
-        # if not np.array_equal(np.sum(matrix, axis=0), np.ones_like(np.sum(matrix, axis=0))) or not np.array_equal(np.sum(matrix, axis=1), np.ones_like(np.sum(matrix, axis=1))):
-        #     continue
-        yield matrix
+from sequential_auction import MultiAuction, solve_naively
 
 def gen_perms_of_perms(curr_perm_list, n, T):
     global total_perm_list
@@ -35,49 +15,82 @@ def gen_perms_of_perms(curr_perm_list, n, T):
         for perm in itertools.permutations(range(n)):
             gen_perms_of_perms(curr_perm_list + [perm], n, T)
 
-if __name__ == "__main__":
-    #Aims to compute a true optimal solution via exhaustive search.
-
-    n = 3
-    m = 3
-    T = 2
+def find_true_optimal_ass(benefit, init_ass, lambda_):
+    n = benefit.shape[0]
+    m = benefit.shape[1]
+    T = benefit.shape[2]
 
     global total_perm_list
     total_perm_list = []
     gen_perms_of_perms([], n, T)
-    print(total_perm_list)
+    
+    best_benefit = -np.inf
+    best_assignment = None
 
-    # benefit = np.random.rand(n,m,T)
-    # init_ass = np.array([[0, 0, 0, 1],
-    #                      [0, 0, 1, 0],
-    #                      [0, 1, 0, 0],
-    #                      [1, 0, 0, 0]])
-    # benefit[:,:,0] = np.array([[100, 1, 0, 0],
-    #                            [1, 100, 0, 0],
-    #                            [0, 0, 0.1, 0.2],
-    #                            [0, 0, 0.2, 0.1]])
-    # benefit[:,:,1] = np.array([[1000, 1, 0, 0],
-    #                            [1, 1000, 0, 0],
-    #                            [0, 0, 0.3, 0.1],
-    #                            [0, 0, 0.1, 0.3]])
-    # lambda_ = 1
+    for perm_list in total_perm_list:
+        assignment_list = []
+        for k, perm in enumerate(perm_list):
+            ass = np.zeros((n,m))
+            for i, j in enumerate(perm):
+                ass[i,j] = 1
+            assignment_list.append(ass)
 
-    # best_benefit = -np.inf
-    # best_assignment = None
-    # #generate all possible assignments
-    # for assignments in generate_matrices(n,m,T):
-    #     assignment_list = [assignments[:,:,j] for j in range(T)]
+        total_benefit = 0
+        for j, ass in enumerate(assignment_list):
+            total_benefit += (benefit[:,:,j]*ass).sum()
 
-    #     total_benefit = 0
-    #     for j, ass in enumerate(assignment_list):
-    #         total_benefit += (benefit[:,:,j]*ass).sum()
+        total_benefit -= calc_handover_penalty(init_ass, assignment_list, lambda_)
 
-    #     total_benefit -= calc_handover_penalty(init_ass, assignment_list, lambda_)
+        if total_benefit > best_benefit:
+            best_benefit = total_benefit
+            best_assignment = assignment_list
 
-    #     if total_benefit > best_benefit:
-    #         best_benefit = total_benefit
-    #         best_assignment = assignment_list
+    return best_benefit, best_assignment
 
-    # print(f"Best Assignment: (for {best_benefit} benefit):")
-    # for ass in best_assignment:
-    #     print(ass)
+if __name__ == "__main__":
+    #Aims to compute a true optimal solution via exhaustive search.
+
+    n = 5
+    m = 5
+    T = 3
+
+    init_ass = None
+    
+    lambda_ = 1
+
+    avg_best = 0
+    avg_smghl = 0
+    avg_smgh = 0
+    avg_naive = 0
+
+    num_avgs = 50
+    for _ in range(num_avgs):
+        benefit = np.random.rand(n,m,T)
+
+        print(f"Run {_}/{num_avgs}", end='\r')
+
+        #SMHGL centralized, lookahead = 2
+        multi_auction = MultiAuction(benefit, None, T, lambda_=lambda_)
+        multi_auction.run_auctions()
+        ben, nh = multi_auction.calc_benefit()
+        avg_smghl += ben/num_avgs
+
+        #SMGH
+        multi_auction = MultiAuction(benefit, None, 1, lambda_=lambda_)
+        multi_auction.run_auctions()
+        ben, nh = multi_auction.calc_benefit()
+        avg_smgh += ben/num_avgs
+
+        #Naive
+        ben, nh = solve_naively(benefit, lambda_)
+        avg_naive += ben/num_avgs
+
+        #Optimal
+        ben, _ = find_true_optimal_ass(benefit, init_ass, lambda_)
+        avg_best += ben/num_avgs
+
+    fig = plt.figure()
+    plt.bar(["Naive","SMGH", "SMGHL", "Optimal"], [avg_naive, avg_smgh, avg_smghl, avg_best])
+    plt.title(f"Average benefit across {num_avgs} runs, n={n}, m={m}, T={T}")
+
+    plt.show()
